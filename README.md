e691f1ba8c4cba3850796660b2412e9cae7a8a57
srb3 <sbrown@chef.io>
2020-10-23 15:08:15 +0100

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# consul_wrapper
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,13 @@
+name 'consul_wrapper'
+maintainer 'Steve Brown'
+maintainer_email 'sbrown@chef.io'
+license 'Apache-2.0'
+description 'Installs/Configures consul_wrapper'
+long_description 'Installs/Configures consul_wrapper'
+version '0.1.0'
+chef_version '>= 14.0'
+
+
+
+issues_url 'https://github.com/srb3/consul_wrapper/issues'
+source_url 'https://github.com/srb3/consul_wrapper'
