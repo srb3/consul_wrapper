419dac1ad53e017a0e20bc99564e0157af841256
srb3 <sbrown@chef.io>
2020-10-23 23:05:53 +0100

    fix windows bin path

--- recipes/service.rb
+++ recipes/service.rb
@@ -1,6 +1,6 @@
 windows_service 'consul' do
   action :create
-  binary_path_name "#{node['consul_wrapper']['bin_file_path']} agent -config-dir=#{node['consul_wrapper']['config_path']} start=auto"
+  binary_path_name "#{node['consul_wrapper']['bin_file_path']}/consul agent -config-dir=#{node['consul_wrapper']['config_path']} start=auto"
   display_name 'consul'
   startup_type :automatic
 end
